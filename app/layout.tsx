ab80bf775e50fb454eeb4cbcdef0dcb6f6b28860
yogirajzala369 <yogirajzala31@gmail.com>
2023-05-15 19:23:14 -0700

    feat-TMDB: Added chakraUI to the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { ChakraProvider } from "@chakra-ui/react";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ChakraProvider>{children}</ChakraProvider>
+      </body>
     </html>
   );
 }
